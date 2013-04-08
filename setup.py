4c2dc1501025e8f322fe9d874c30ce78281f65da
emre <mail@emreyilmaz.me>
2013-04-09 00:10:59 +0300

    setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='redis-router',
+    version='0.1',
+    packages=['redis_router'],
+    url='https://github.com/emre/redis-router',
+    license='MIT',
+    author='Emre Yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='A redis sharding library/api for your sharding needs.',
+)
