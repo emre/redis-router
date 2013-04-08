4c78b22b67ed8e161c72ca367e1d1327a2a90a4f
emre <mail@emreyilmaz.me>
2013-04-08 20:02:24 +0300

    * Added HTTP interface w/ flask and gevent. * Added example file for the HTTP interface

---
+++ examples/http_server.py
@@ -0,0 +1,5 @@
+# -*- coding: utf8 -*-
+
+from redis_router.http_interface import start_server
+
+start_server('0.0.0.0', 5000)
---
+++ redis_router/http_interface.py
@@ -0,0 +1,31 @@
+# -*- coding: utf8 -*-
+
+from flask import Flask, render_template, jsonify, request
+from router import Router
+
+import os
+
+# initialize flask application
+app = Flask(__name__)
+
+config_file = os.getenv('ROUTER_CONFIG_FILE', '/etc/redis_router/servers.config')
+
+# main view
+@app.route('/', methods=['POST', ])
+def index():
+    router = Router(config_file)
+    command, arguments = request.form['command'], request.form['arguments']
+
+    arguments = arguments.split(",")
+    router_response = getattr(router, command)(*arguments)
+    if isinstance(router_response, set):
+        router_response = list(router_response)
+
+    return jsonify({"response": router_response})
+
+from gevent.wsgi import WSGIServer
+
+
+def start_server(host, port):
+    http_server = WSGIServer((host, port), app)
+    http_server.serve_forever()
